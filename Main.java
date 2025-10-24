4151dedcf439a51a19badc98c66b57e24fe131a9
numeroab <renanfelipefranco@gmail.com>
2025-10-24 11:56:16 -0300

    completo

--- Main.java
+++ Main.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner ler = new Scanner(System.in);
+    int faixa;
 
     public static void main(String[] args) {
         String enderecoIP;
         String classe = "";
-        String status;
+        String[] saida = new String[2];
         System.out.print("Digite um endereco IP: ");
         enderecoIP = ler.nextLine();
         String[] octetos = enderecoIP.split("\\.");
+        int i = 0;
+
         if (validar(octetos)) {
             System.out.println("Endereco IP valido.");
             if (Integer.parseInt(octetos[0]) < 128) {
@@ -22,56 +25,77 @@ public class Main {
                 classe = "C";
 
             }
+            if (classe == "A") {
+                i = 1;
+            }
+            if (classe == "B") {
+                i = 2;
+            }
+            if (classe == "C") {
+                i = 3;
+            }
             System.out.println("Classe: " + classe);
-            System.out.println(determinarStatus(octetos, classe));
-
+            System.out.println(determinarStatus(octetos, classe, i));
+            saida = determinarFaixa(i, octetos);
+            System.out.println(saida[0]);
+            System.out.println(saida[1]);
         } else {
             System.out.println("Endereco IP invalido.");
         }
 
     }
 
-   
-        public static boolean validar(String[] octetos) {
-            if (octetos.length != 4) {
+    public static boolean validar(String[] octetos) {
+        if (octetos.length != 4) {
+            return false;
+        }
+        for (int i = 0; i < octetos.length; i++) {
+            int valor = Integer.parseInt(octetos[i]);
+            if (valor > 255 || valor < 0) {
                 return false;
             }
-            for (int i = 0; i < octetos.length; i++) {
-                int valor = Integer.parseInt(octetos[i]);
-                if (valor > 255 || valor < 0) {
-                    return false;
-                }
 
-            }
+        }
 
-            return true;
-        
+        return true;
+
     }
-    public static String determinarStatus(String[] octetos, String classe) {
-        int i = 0;
-        if (classe == "A") {
-            i = 1;
-        }
-        if (classe == "B") {
-            i = 2;
-        }
-        if (classe == "C") {
-            i = 3;
-        }
-     
-            for (; i < octetos.length; i++) {
-                if (Integer.parseInt(octetos[i]) != 0 && Integer.parseInt(octetos[i]) != 255) {
-                 return "Host";
-                    
-                } else if (Integer.parseInt(octetos[i]) == 0){
-                    return "Rede";
-                }
-                 else{
-                    return "Broadcast";
-                }
+
+    public static String determinarStatus(String[] octetos, String classe, int i) {
+        String retorno = "erro";
+
+        for (int j = i; j < octetos.length; j++) {
+            if (Integer.parseInt(octetos[j]) != 0 && Integer.parseInt(octetos[i]) != 255) {
+                retorno = "Host";
+                break;
+            } else if (Integer.parseInt(octetos[j]) == 0) {
+                retorno = "rede";
+
+            } else {
+                retorno = "Broadcast";
             }
-        
-       
-        return "Erro";
+        }
+        return retorno;
+
+        // return "Erro";
+    }
+
+    public static String[] determinarFaixa(int i, String[] octetos) {
+        String[] faixa = new String[2];
+        faixa[1] = "";
+        faixa[0] = "";
+        for (int j = 0; j < i; j++) {
+            faixa[0] += octetos[j] + ".";
+            faixa[1] += octetos[j] + ".";
+        }
+        for (; i < 3; i++) {
+
+            faixa[0] += "0" + ".";
+            faixa[1] += "255" + ".";
+        }
+        faixa[0] += "1";
+        faixa[1] += "254";
+        return faixa;
+
     }
 }
